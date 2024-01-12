289f0d9c30c33ff365f25fc121e7b9c05f22507a
Harshath <harshathmkulal@gmail.com>
2024-01-12 22:41:57 +0530

    Create queue.py

---
+++ Data_Structure/queue.py
@@ -0,0 +1,32 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            print("Queue is empty.")
+
+    def size(self):
+        return len(self.items)
+
+# Example usage:
+my_queue = Queue()
+
+my_queue.enqueue(1)
+my_queue.enqueue(2)
+my_queue.enqueue(3)
+
+print("Queue size:", my_queue.size())
+
+print("Dequeue:", my_queue.dequeue())
+print("Dequeue:", my_queue.dequeue())
+
+print("Queue size after dequeues:", my_queue.size())
